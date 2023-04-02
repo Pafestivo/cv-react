7a4f782e76f402ece4512d7a3ee0e10ad454cd32
Or Rodrigez <orrodrigez1@gmail.com>
2023-04-02 16:00:40 +0300

    Style the preview

--- src/components/preview/PrevContainer.js
+++ src/components/preview/PrevContainer.js
@@ -6,13 +6,34 @@ class PrevContainer extends Component {
     const { firstName, lastName, role, address, number, email, description} = this.props.cv.personalInfo
     return(
       <div className="prev-container">
-        <h1>{firstName} {lastName}</h1>
-        <h3>{role}</h3>
-        <p>{address}</p>
-        <p>{number}</p>
-        <p>{email}</p>
-        <p>{description}</p>
-      </div>
+        <div className="name-role">
+          <h1>{firstName} {lastName}</h1>
+          <h3>{role}</h3>
+        </div>
+        <div className="grid-container">
+            <div className="personal-details">
+            <h3>Personal Details</h3>
+            <div className="address">
+              <h4>Address</h4>
+              <p>{address}</p>
+            </div>
+            <div className="number">
+              <h4>Phone Number</h4>
+              <p>{number}</p>
+            </div>
+            <div className="email">
+              <h4>Email</h4>
+              <p>{email}</p>
+            </div>
+          </div> {/* personal-details */}
+
+          <div className="description">
+            <h2>Description</h2>
+            <p>{description}</p>
+          </div>      
+        </div>
+
+      </div> /* prev-container */
     )
   }
 }
